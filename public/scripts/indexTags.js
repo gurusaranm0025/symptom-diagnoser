ffca0c3defd995d9bda702e50b3ccb868d4a929b
Guru Saran M <gurusaranm0025@gmail.com>
2023-09-30 00:55:15 +0530

    now we can press enter for submition and prediction of the disease

--- public/scripts/indexTags.js
+++ public/scripts/indexTags.js
@@ -1,5 +1,5 @@
 // Tags for input code
-
+const submitBtn = document.querySelector(".submit-btn")
 const input_tag = document.querySelector('.input-tag')
 const tags_length = document.querySelector('.tags-length')
 
@@ -8,21 +8,25 @@ input_tag.addEventListener("keyup", (e) => {
     const val = input_tag.value
 
     if (e.key === "Enter") {
-
-        if(tags_list.some(e => e.text == val)) return alert('Duplicate tags')
-        if (val == '') return;
-
-        const tags = val.split(',').map(e => e.trim()).filter(e => e !== "")
-
-        for (let i of tags) {
-            tags_list.push({
-                id: Math.random().toString(10).substring(2, 10),
-                text: i
-            })
+        if (tags_list.length > 0 && val == '') {
+            submitBtn.click();
+        } else {
+            if(tags_list.some(e => e.text == val)) return alert('Duplicate tags')
+            if (val == '') return;
+    
+            const tags = val.split(',').map(e => e.trim()).filter(e => e !== "")
+    
+            for (let i of tags) {
+                tags_list.push({
+                    id: Math.random().toString(10).substring(2, 10),
+                    text: i
+                })
+            }
+    
+            input_tag.value = ""
+            console.log(input_tag)
+            RenderTags()
         }
-
-        input_tag.value = ""
-        RenderTags()
     }
 })
 
@@ -37,7 +41,7 @@ function RenderTags() {
         cache = `<div class="item-tag">
                     <span>${e.text}</span>
                     <button type="button" data-id="${e.id}" class="btn-rm-tag">
-                        <i class="fas fa-times"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
                     </button>
                 </div>`;
         wrapper_tags.insertAdjacentHTML('afterbegin', cache)
